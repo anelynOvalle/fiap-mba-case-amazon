a6c4d51a8cc7edf5a90ec42d6f62006d521f09a8
Anelyn <anelyn-ovalle@hotmail.com>
2020-10-17 18:43:37 -0300

    estrutura inicial do projeto

---
+++ services-architecture/order-amazon-service/src/main/java/com/fiap/ralfmed/orderamazonservice/OrderAmazonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.ralfmed.orderamazonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderAmazonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderAmazonServiceApplication.class, args);
+	}
+
+}
---
+++ services-architecture/product-amazon-service/src/main/java/com/fiap/ralfmed/productamazonservice/ProductAmazonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.ralfmed.productamazonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAmazonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAmazonServiceApplication.class, args);
+	}
+
+}
